26ecc3e0862948203f93d7a3bafe30887975ad1e
Natalya <kim87324@yandex.ru>
2024-10-06 18:12:38 +0300

    add update and reset state

--- script.js
+++ script.js
@@ -22,6 +22,16 @@ function onAddItemSubmit(evt) {
     return;
   }
 
+  //Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  }
+
   //Create item DOM element
   addItemToDOM(newItem);
 
@@ -151,6 +161,8 @@ function filterItems(evt) {
 }
 
 function checkUI() {
+  itemInput.value = '';
+
   const items = itemList.querySelectorAll('li');
 
   if (items.length === 0) {
@@ -160,6 +172,11 @@ function checkUI() {
     clearBtn.style.display = 'block';
     itemFilter.style.display = 'block';
   }
+
+  formBtn.innerHTML = `<i class='fa-solid fa-plus'></i> Add Item`;
+  formBtn.style.backgroundColor = '#333';
+
+  isEditMode = false;
 }
 
 //Initialize app
